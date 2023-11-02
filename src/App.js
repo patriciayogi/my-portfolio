2f2631e021819fe065ead7992999d73090b59199
pyogi <pyogi@stellar.aero>
2023-11-02 16:06:54 -0700

    initial structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 export default function App() {
-  return "App";
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />      
+      <Skills />
+      <Projects />      
+      <Contact />
+    </main>
+  );
 }
