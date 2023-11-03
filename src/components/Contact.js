72403034ff8b753d1e88998fc6c45bc59adbd297
pyogi <pyogi@stellar.aero>
2023-11-03 12:19:45 -0700

    contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -20,7 +20,7 @@ export default function Contact() {
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
             body: encode({ "form-name": "contact", name, email, message }),
         })
-            .then(() => alert("Thank you!"))
+            .then(() => alert("Thank yo!"))
             .catch((error) => alert(error));
     }
 
@@ -45,34 +45,32 @@ export default function Contact() {
                                 ADDRESS
                             </h2>
                             <p className="mt-1">                                
-                                Greater Vancouver-BC, Canada
+                                Greater Vancouver, British Columbia, Canada
                             </p>
                         </div>
                         <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                                 EMAIL
                             </h2>
-                            <a className="text-indigo-400 leading-relaxed">
-                                patricia.yogi@email.com
-                            </a>
+                            <a className="text-indigo-400 leading-relaxed" href = "mailto: patricia.yogi@email.com">patricia.yogi@email.com</a>                            
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                                 PHONE
                             </h2>
-                            <p className="leading-relaxed">778-798-4214</p>
+                            <p className="leading-relaxed" href = "tel:778-798-4214">778-798-4214</p>
                         </div>
                     </div>
                 </div>
                 <form
-                    netlify
-                    name="contact"
+                    name="contact" 
+                    method="POST" 
+                    data-netlify="true"
                     onSubmit={handleSubmit}
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                     <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-                        Hire Me
+                        Let's connect
                     </h2>
                     <p className="leading-relaxed mb-5">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-                        suscipit officia aspernatur veritatis. Asperiores, aliquid?
+                        Do you have any question? I look forward to hearing from you
                     </p>
                     <div className="relative mb-4">
                         <label htmlFor="name" className="leading-7 text-sm text-gray-400">
