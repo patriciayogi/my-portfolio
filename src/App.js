00d44ea1519ffe1311931a17a56c6a674744dc24
patricia.yogi@gmail.com <patricia.yogi@gmail.com>
2023-11-18 19:51:39 -0800

    github pages

--- src/App.js
+++ src/App.js
@@ -7,18 +7,17 @@ import NoPage from "./components/NoPage";
 import Skills from "./components/Skills";
 import SecretSantaApp from "./components/projects/SecretSantaApp";
 
-
-
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navbar />}>
-        <Route index element={<Main />} /> 
+          <Route index element={<Main />} />
           {/* */}
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/secretsanta" element={<SecretSantaApp />} />
-        <Route path="*" element={<NoPage />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/my-portfolio" element={<Main />} />
+          <Route path="/secretsanta" element={<SecretSantaApp />} />
+          <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
